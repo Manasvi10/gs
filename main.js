a25e76c8ebdfac4d0a7d5dedd7a70389293dc065
Manasvi10 <86060802+Manasvi10@users.noreply.github.com>
2022-01-20 20:20:55 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+function multiplyBy()
+{
+   num1 = document.getElementById("number1").value;
+   num2 = document.getElementById("number2").value;
+   document.getElementById("answer").innerHTML = num1 * num2;
+
+
+   
+}
